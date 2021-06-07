ed7f8e65d5e9f613c7ea2d2f59a7ef9e0febe12b
Vladimir S <46943806+xk1b0r9x@users.noreply.github.com>
2021-06-07 18:15:15 +0300

    feat: added a new exercise code

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -2,7 +2,24 @@ import Foundation
 
 public extension Int {
     
-    var roman: String? {
-        nil
+        var roman: String? {
+        var mySelf = self
+        if mySelf <= 0 && mySelf > 3999 {
+        return nil
+        }
+        var nameString = ""
+        let listInt = [100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M", 10: "X", 40: "XL", 50: "L", 90: "XC", 1: "I", 4: "IV", 5: "V", 9: "IX"].sorted(by: >)
+        for i in listInt{
+            while mySelf >= i.0 {
+                nameString += i.1
+                mySelf -= i.0
+            }
+        }
+        
+        if nameString == "" {
+            return nil
+        }
+        return nameString
     }
+
 }
